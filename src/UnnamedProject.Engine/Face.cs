8d3b058aab60077ba460e652f9bc7e3a90cc97dd
Scott Holden <scott@nullops.io>
2020-01-09 23:34:09 +1100

    Moved VectorFace into its own file.

--- src/UnnamedProject.Engine/Face.cs
+++ src/UnnamedProject.Engine/Face.cs
@@ -1,6 +1,4 @@
-﻿using System.Numerics;
-
-namespace UnnamedProject.Engine
+﻿namespace UnnamedProject.Engine
 {
     public struct Face
 	{
@@ -9,20 +7,6 @@ namespace UnnamedProject.Engine
         public int C { get; }
         public Color Color { get; }
         public Face(int a, int b, int c, Color color)
-        {
-            this.A = a;
-            this.B = b;
-            this.C = c;
-            this.Color = color;
-        }
-    }
-    public struct VectorFace
-    {
-        public Vector3 A { get; }
-        public Vector3 B { get; }
-        public Vector3 C { get; }
-        public Color Color { get; }
-        public VectorFace(Vector3 a, Vector3 b, Vector3 c, Color color)
         {
             this.A = a;
             this.B = b;
---
+++ src/UnnamedProject.Engine/VectorFace.cs
@@ -0,0 +1,19 @@
+﻿using System.Numerics;
+
+namespace UnnamedProject.Engine
+{
+    public struct VectorFace
+    {
+        public Vector3 A { get; }
+        public Vector3 B { get; }
+        public Vector3 C { get; }
+        public Color Color { get; }
+        public VectorFace(Vector3 a, Vector3 b, Vector3 c, Color color)
+        {
+            this.A = a;
+            this.B = b;
+            this.C = c;
+            this.Color = color;
+        }
+    }
+}
