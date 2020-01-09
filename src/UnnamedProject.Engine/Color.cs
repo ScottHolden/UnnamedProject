26a90bfb10e9b7222c6073fe54bf89d324bb0333
Scott Holden <scott@nullops.io>
2020-01-09 23:32:56 +1100

    Implemented Equals for Color

--- src/UnnamedProject.Engine/Color.cs
+++ src/UnnamedProject.Engine/Color.cs
@@ -1,7 +1,9 @@
-﻿namespace UnnamedProject.Engine
+﻿using System;
+
+namespace UnnamedProject.Engine
 {
-    public struct Color
-	{
+    public struct Color : IEquatable<Color>
+    {
         public byte R { get; }
         public byte G { get; }
         public byte B { get; }
@@ -12,6 +14,28 @@
             this.G = g;
             this.B = b;
             this.A = a;
+        }
+
+        // Let's be nice struct citizens 
+        public override bool Equals(object? obj) => obj is Color color && Equals(color);
+        public bool Equals(Color other) =>
+            this.R == other.R &&
+            this.G == other.G &&
+            this.B == other.B &&
+            this.A == other.A;
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(this.R, this.G, this.B, this.A);
+        }
+
+        public static bool operator ==(Color left, Color right)
+        {
+            return left.Equals(right);
+        }
+
+        public static bool operator !=(Color left, Color right)
+        {
+            return !(left == right);
         }
     }
 }
